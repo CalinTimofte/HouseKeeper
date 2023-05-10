fce75bfd9af5c04742d8c80f91353ac04ad0ce82
Calin Timofte <calin.timofte@yahoo.com>
2023-05-11 00:01:47 +0200

    feature: add and delete foodItem features are added to frontend, app is now kinda usable

--- pages/storage/[name].js
+++ pages/storage/[name].js
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 import styles from '../../styles/Home.module.scss';
+import { useRouter } from 'next/router';
+
 
 // Generates storage/fridge, /pantry, /cabinet
 export async function getStaticPaths() {
@@ -30,30 +32,78 @@ export async function getStaticProps(context) {
   }
 }
 
+
 const Storage = ({storage, storageName}) => {
+  const router = useRouter();
+  const refreshData = () => {
+    router.replace(router.asPath);
+  }
+
+  const deleteButtonAction = async (foodItemName) => {
+    fetch('http://localhost:3000/api/deleteFoodItem', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+      name: foodItemName
+    })}).then(refreshData())
+  };
+
+  const createButtonAction = async () => {
+    fetch('http://localhost:3000/api/addFoodItem', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({
+        "name": "cornflakes",
+        "expirationDate": "2024-04-20",
+        "quantity": 1,
+        "unit": "piece"
+    })})
+    .then(res => {
+      fetch('http://localhost:3000/api/addFoodItemToStorage', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+          "storage": storageName,
+          "foodItem": "cornflakes"
+      })})
+    })
+    .then(refreshData())
+  };
+
   const storageListItems = storage.foodArray.map(item =>
     <li key={item.id} className={styles.card}>
         <h3>{item.name}</h3>
+        <h3 className={styles.button_danger} onClick={() => deleteButtonAction(item.name)}>- delete</h3>
     </li>)
-return (
-    <div className={styles.container}>
-        <Head>
-            <title>{storageName} | HouseKeeper App</title>
-            <link rel="icon" href="/favicon.ico" />
-        </Head>
 
-        <main>
-            <h1 className={styles.title}>
-              {storageName} | HouseKeeper
-            </h1>
+  return (
+      <div className={styles.container}>
+          <Head>
+              <title>{storageName} | HouseKeeper App</title>
+              <link rel="icon" href="/favicon.ico" />
+          </Head>
 
-            <ul className={styles.grid}>{storageListItems}
-            <p></p>
-            </ul>
-        </main>
+          <main>
+              <h1 className={styles.title}>
+                {storageName} | HouseKeeper
+              </h1>
 
-    </div>
-  )
+              <ul className={styles.grid}>
+                {storageListItems}
+              </ul>
+              <div className={styles.button_create}>
+                  <h3 onClick={createButtonAction}> + add food </h3>
+              </div>
+          </main>
+
+      </div>
+    )
 }
 
 export default Storage
